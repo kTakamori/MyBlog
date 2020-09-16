0bf8247c70fcafef62acf6ea73a5de73efa339fc
EC2 Default User <ec2-user@ip-172-31-16-29.us-east-2.compute.internal>
2020-09-16 11:28:55 +0000

    Add postgresql

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
-class Message < 
+class Message < ApplicationRecord
     validates :title , length: { maximum: 32 } , presence: true
     validates :message , length: { maximum: 256 } , presence: true
     validates :name , length: { maximum: 32 }
